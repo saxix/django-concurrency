b4c9d64c5a1556dc8274fa226d5bedfe03d8dc3a
sax <s.apostolico@gmail.com>
2013-02-16 11:28:00 +0700

    send 'got_request_exception' signal in middleware

--- concurrency/middleware.py
+++ concurrency/middleware.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.core.signals import got_request_exception
 from django.core.urlresolvers import get_callable
 from concurrency.core import RecordModifiedError
 from concurrency.views import handler409
@@ -9,6 +10,6 @@ from concurrency.views import handler409
 class ConcurrencyMiddleware(object):
     def process_exception(self, request, exception):
         if isinstance(exception, RecordModifiedError):
+            got_request_exception.send(sender=self, request=request)
             callback = get_callable(handler409)
-
             return callback(request, target=exception.target)
