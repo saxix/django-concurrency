4a3475aeac3d95c964423ea45fb0bbe89f890034
sax <s.apostolico@gmail.com>
2013-03-01 20:06:42 +0700

    removed django.contrib.admindocs from test INSTALLED_APPS

--- concurrency/tests/contrib_admin.py
+++ concurrency/tests/contrib_admin.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.admindocs',
+    # 'django.contrib.admindocs',
     'django.contrib.admin',
     'concurrency')
 
