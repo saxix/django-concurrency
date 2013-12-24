0b1a494ad8d264d14e92aef188fb48db5034e24c
sax <s.apostolico@gmail.com>
2013-12-24 18:54:49 +0100

    fixes demoproject

--- demo/manage.py
+++ demo/manage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import os, sys
+here =  os.path.abspath(os.path.join(os.path.dirname(__file__)))
+rel = lambda *args: os.path.join(here, *args)
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
+sys.path.insert(0, rel(os.pardir))
 
 
 if __name__ == "__main__":
