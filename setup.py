b37c44d518e50301b61c4398c305981e4bdbe27b
sax <s.apostolico@gmail.com>
2014-01-02 16:26:14 +0100

    updates package files

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 import concurrency
 
 NAME = 'django-concurrency'
@@ -18,9 +18,7 @@ setup(
     url='https://github.com/saxix/django-concurrency',
     author='Stefano Apostolico',
     author_email='s.apostolico@gmail.com',
-    packages=["concurrency",
-              "concurrency.templatetags"
-    ],
+    packages=find_packages(),
     include_package_data=True,
     description="Optimistic lock implementation for Django. Prevents users from doing concurrent editing.",
     long_description=open('README.rst').read(),
