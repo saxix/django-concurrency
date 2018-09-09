04c7e0c914a738ccada2af05cec63b1e45889456
sax <s.apostolico@gmail.com>
2018-09-09 23:32:01 +0200

    restores support django>=1.10

--- src/concurrency/compat.py
+++ src/concurrency/compat.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
+import django
 
-# try:
+# if django.VERSION <(1,9):
 #     from django.template.base import TemplateDoesNotExist
-# except ImportError:
+# else:
 from django.template.exceptions import TemplateDoesNotExist  # noqa - django 1.9
 
-# try:
-from django.urls.utils import get_callable
-# except ImportError:
+# if django.VERSION <(1,9):
 #     from django.core.urlresolvers import get_callable  # noqa
+# else:
+from django.urls.utils import get_callable
